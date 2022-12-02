5a56bcd9350eb50b9f2b2b40f11090e2a7c05c35
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-12-02 09:48:17 +0200

    task-1 in progress

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -1,30 +1,59 @@
-const Feedback = () => {
-  return (
-    <div className="feedback">
-      <h1 className="head__feedback">Please leave feedback</h1>
-      <div className="button">
-        <ul className="button__list">
-          <li className="button__item">
-            <button type="button">Good</button>
-          </li>
-          <li className="button__item">
-            <button type="button">Neutral</button>
-          </li>
-          <li className="button__item">
-            <button type="button">Bad</button>
-          </li>
-        </ul>
-      </div>
-      <div className="statistics">
-        <h2 className="head__stat">Statistics</h2>
-        <ul className="stat__list">
-          <li className="stat__item">good:</li>
-          <li className="stat__item">neutral:</li>
-          <li className="stat__item">bad:</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+import React from 'react';
+import { Component } from 'react';
+
+import { FeedbackForm, ButtonList } from 'components/feedback/feedback.styled';
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+
+  handleNeutral = () => {};
+
+  handleBad = () => {};
+
+  render() {
+    return (
+      <FeedbackForm>
+        <h1 className="head__feedback">Please leave feedback</h1>
+        <div className="button">
+          <ButtonList>
+            <li className="button__item">
+              <button type="button" onClick={this.handleGood}>
+                Good
+              </button>
+            </li>
+            <li className="button__item">
+              <button type="button" onClick={this.handleNeutral}>
+                Neutral
+              </button>
+            </li>
+            <li className="button__item">
+              <button type="button" onClick={this.handleBad}>
+                Bad
+              </button>
+            </li>
+          </ButtonList>
+        </div>
+        <div className="statistics">
+          <h2 className="head__stat">Statistics</h2>
+          <ul className="stat__list">
+            <li className="stat__item">good:{this.state.good}</li>
+            <li className="stat__item">neutral:{this.state.neutral}</li>
+            <li className="stat__item">bad:{this.state.bad}</li>
+          </ul>
+        </div>
+      </FeedbackForm>
+    );
+  }
+}
 
 export default Feedback;
---
+++ src/components/feedback/feedback.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const FeedbackForm = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const ButtonList = styled.div`
+  display: flex;
+  list-style: none;
+  gap: 5px;
+`;
